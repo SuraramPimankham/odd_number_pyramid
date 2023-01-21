ca1afb0612d7b2dcda7cd04b1156706dbe29f065
Suraram Pimankham <76487588+SuraramPimankham@users.noreply.github.com>
2023-01-21 23:21:34 +0700

    Initial commit

---
+++ odd_number_pyramid/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace odd_number_pyramid
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+            int Space, Number;
+
+            for (int i = 1; i <= input; i++)  
+            {
+                for (Space = 1; Space <= (input - i); Space++)
+                { 
+                    Console.Write(" ");
+                }
+                for (Number = 1; Number <= i; Number++)
+                {
+                    Console.Write("*"); 
+                }
+                for (Number = (i - 1); Number >= 1; Number--)
+                { 
+                    Console.Write("*"); 
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
